cf9a356e5e70b347f4e581446745c59d6fd3a891
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2024-02-24 18:34:25 +0100

    Renaming :rocket:

--- android/app/src/main/java/com/dailydeals/MainActivity.kt
+++ android/app/src/main/java/com/dailydeals/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dailydealsskia
+package com.dailydeals
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "DailyDealsSkia"
+  override fun getMainComponentName(): String = "DailyDeals"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/dailydeals/MainApplication.kt
+++ android/app/src/main/java/com/dailydeals/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dailydealsskia
+package com.dailydeals
 
 import android.app.Application
 import com.facebook.react.PackageList
